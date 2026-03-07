39d5a01bfd605ffd2a66d1c0e7d22db6e8b97a91
hc <hanchengyang@gmail.com>
2026-03-07 18:02:10 +0800

    Create DocumentStatistics.py
    Check the statistics of the files

---
+++ DocumentStatistics.py
@@ -0,0 +1,40 @@
+import os
+import re
+import fitz
+import numpy as np
+
+folder = os.path.join("Data", "PDFs")
+
+word_counts = []
+sentence_counts = []
+token_counts = []
+
+for file in os.listdir(folder):
+    if file.endswith(".pdf"):
+        path = os.path.join(folder, file)
+
+        doc = fitz.open(path)
+        text = ""
+
+        for page in doc:
+            text += page.get_text()
+
+        words = re.findall(r"\w+", text)
+        sentences = re.split(r"[.!?]", text)
+
+        word_counts.append(len(words))
+        sentence_counts.append(len([s for s in sentences if s.strip()]))
+
+# Document statistics
+print("Document Statistics")
+print("Min words:", min(word_counts))
+print("Max words:", max(word_counts))
+print("Mean words:", int(np.mean(word_counts)))
+print("Median words:", int(np.median(word_counts)))
+
+print()
+
+# Token statistics
+print("Token Statistics")
+print("Avg tokens per document:", int(np.mean(word_counts)))
+print("Avg sentences per document:", int(np.mean(sentence_counts)))
